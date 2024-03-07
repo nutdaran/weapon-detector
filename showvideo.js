0efcc1e10abd331ec0669505ffc8e800d980be58
Piyachon1234 <73595597+Piyachon1234@users.noreply.github.com>
2024-03-07 11:48:35 +0700

    Add files via upload

---
+++ showvideo.js
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>video</title>
+    </head>
+    <body>
+        <video id="videoPlayer" controls autoplay>
+            <source src="video.m3u8" type="video/m3u8">
+
+            </source>
+        </video>
+        <script>
+            var video = document.getVideoById('video');
+        </script>
+    </body>
+</html>
